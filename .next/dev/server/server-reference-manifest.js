ecc7a9778c1fe10e52d5e6da505f57a868771469
alblancosr-a11y <alblancosr@gmail.com>
2026-09-20 00:00:40 +0300

    Ignore Next.js build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4D00/EFj/47B7KjSyC/MuwIRu13l+HVX50IRAzrQRZs=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_0-ri78n._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_04.vw6j._.js","static/chunks/node_modules_12dd1ry._.js"],
-    source: "dynamic"
-});
